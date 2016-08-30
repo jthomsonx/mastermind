48f9a488f0f35e506e6c63b9646f002d60d13db6
James Thomson <jthomsonx@gmail.com>
2016-08-30 21:35:53 +0200

    added computer play functionality (non-AI)

--- mastermind.rb
+++ mastermind.rb
@@ -7,11 +7,9 @@
 
 class Game
 	attr_reader :code, :answer
-	Player = Struct.new(:name)
+	Player = Struct.new(:name, :type)
 
 	def initialize
-		@codemaker = Player.new("Codemaker")
-		@codebreaker = Player.new("Codebreaker")
 		@counter = 1
 		@code = Code.new()
 		@answer = Array.new()
@@ -19,7 +17,14 @@ class Game
 
 	def play
 		welcome_message
-		@code.create_code
+		player_select
+		instructions
+		if @codemaker.type == :computer
+			@code.computer_code
+		elsif @codemaker.type == :human
+			@code.human_code
+			print_code
+		end
 #		print_code	# For debugging purposes only
 		start_playing
 		show_result
@@ -29,15 +34,45 @@ class Game
 		puts "...................."
 		puts "Welcome to Mastermind!"
 		puts "...................."
-		puts "#{@codebreaker.name}: You will have 12 turns to guess the correct code"
-		puts "You will be told at the end of each turn how many colours you have right,"
-		puts "and how many are in the correct location. Duplicates are allowed."
+	end
+
+	def player_select
+		print "Please enter your name: "
+		name = gets.chomp
+		print "Please type 'A' to be the CodeBreaker, or 'B' for CodeMaker: "
+		type = gets.chomp
+		if type.downcase == "a"
+			@codebreaker = Player.new(name, :human)
+			@codemaker = Player.new("Computer", :computer)
+		else
+			@codemaker = Player.new(name, :human)
+			@codebreaker = Player.new("Computer", :computer)
+		end
+	end
+
+	def instructions
 		puts ""
-		puts "You can select from Blue, Red, Yellow, Green, Purple and Orange"
-		puts "Please make your selection using the following syntax: "
-		puts "Type 'blue red green orange' for Blue, Red, Green and Orange"
 		puts "...................."
-		puts "Let the game begin!"
+		puts "Mastermind is a game of logic, where the objective is to guess a random"
+		puts "code of four colours."
+		puts ""
+		if @codebreaker.type == :human
+			puts "#{@codebreaker.name}: You will have 12 turns to guess the correct code"
+			puts "You will be told at the end of each turn how many colours you have right,"
+			puts "and how many are in the correct location. Duplicates are allowed."
+			puts ""
+			puts "You can select from Blue, Red, Yellow, Green, Purple and Orange"
+			puts "Please make your selection using the following syntax: "
+			puts "e.g. Type 'blue red green orange' for Blue, Red, Green and Orange"
+			puts ""
+		elsif @codemaker.type == :human
+			puts "#{@codemaker.name}: You will enter a secret code of four colours which"
+			puts "#{@codebreaker.name} will have 12 turns to guess correctly."
+			puts ""
+			puts "Please make your code selection using the following syntax: "
+			puts "e.g. Type 'blue red green orange' for Blue, Red, Green and Orange"
+			puts ""
+		end
 	end
 
 	def print_code
@@ -46,6 +81,9 @@ class Game
 	end
 
 	def start_playing
+		puts "...................."
+		puts "Let's start the game!"
+		sleep 2
 		turns until game_over
 	end
 
@@ -57,18 +95,31 @@ class Game
 		else
 			print "Please select four colours: "
 		end
-		@answer = gets.chomp.downcase.split(" ")
-#		puts @answer.inspect # For debugging purposes only
-#		puts @answer.length # For debugging purposes only
+		if @codebreaker.type == :human
+			human_guess
+		elsif @codebreaker.type == :computer
+			computer_guess
+		end
 		if check_for_errors == true
 			@counter += 1
+			check_for_win
+			check_for_match
 		else
 			puts "Try again!"
 		end
-		check_for_win
-		check_for_match
-		#check_for_pos
-		#check_for_col
+		@answer = []
+	end
+
+	def human_guess
+		@answer = gets.chomp.downcase.split(" ")
+	end
+
+	def computer_guess
+		until @answer.length == 4
+			@answer << @code.values.sample
+		end
+		puts @answer.to_s
+		sleep 2		
 	end
 
 	def game_over
@@ -85,6 +136,7 @@ class Game
 			puts "Congratulations #{@winner.name}. You won the game!"
 		else
 			puts "Sorry #{@codebreaker.name}. Better luck next time!"
+			puts "#{@codemaker.name} won this round!"
 		end
 	end
 
@@ -143,10 +195,30 @@ class Game
 			@values = ["blue", "red", "yellow", "green", "purple", "orange"]
 		end
 
-		def create_code
+		def computer_code
 			4.times do |x|
 				x = @values.sample # selects random value from @values array
 				@random << x # push random colour value into @random array
+			end			
+		end
+
+		def human_code
+			while true
+				print "Please select a four colour code. Remember, duplicates are allowed: "
+				@random = gets.chomp.downcase.split(" ")
+#				puts @random.inspect 	# For debugging only
+				return false unless human_code_error_check == true
+			end
+		end
+
+		# Should be able to refactor this along with "check_for_error" method above.
+		def human_code_error_check
+			if @random.length == 4 && (@random - @values).empty?
+				return false
+			else
+				puts "Error: Incorrect number of colours: #{@random.length} instead of 4." unless @random.length == 4 
+				puts "Error: Incorrect colour value(s) chosen!" unless (@random - @values).empty?
+				return true
 			end
 		end
 	end
